8b7206825ab1dc6a8bf93a6f41b2bf2a7e8b0af1
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-01 14:48:28 +0200

    wrote Member.java

--- src/main/java/com/example/Wigell_Admin_Api/entities/Member.java
+++ src/main/java/com/example/Wigell_Admin_Api/entities/Member.java
@@ -1,4 +1,105 @@
 package com.example.Wigell_Admin_Api.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FirstName", length = 50)
+    private String firstName;
+
+    @Column(name = "LastName", length = 50)
+    private String lastName;
+
+    @Column(name = "Address")
+    @ManyToOne
+    private Address address;
+
+    @Column(name = "Email", unique = true, nullable = false, length = 30)
+    private String email;
+
+    @Column(name = "Phone",nullable = true, length = 11)
+    private String phone;
+
+    @Column(name = "Date_Of_Birth", nullable = false, length = 8)
+    private Date dateOfBirth;
+
+    public Member() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address=" + address +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 }
