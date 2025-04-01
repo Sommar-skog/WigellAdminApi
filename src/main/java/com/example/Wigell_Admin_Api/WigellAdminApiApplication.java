7893f3541d0a40c0fb90fb4edc218fdce2475634
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-01 14:06:00 +0200

    Initial commit

---
+++ src/main/java/com/example/Wigell_Admin_Api/WigellAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wigell_Admin_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigellAdminApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WigellAdminApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Wigell_Admin_Api/WigellAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Wigell_Admin_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
