2a70a4ab1f7595317e0e07d929358177fc0fdbb5
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-02 08:57:17 +0200

    Added Address.java

--- src/main/java/com/example/Wigell_Admin_Api/entities/Address.java
+++ src/main/java/com/example/Wigell_Admin_Api/entities/Address.java
@@ -1,4 +1,66 @@
 package com.example.Wigell_Admin_Api.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Street", nullable = false, length = 50)
+    private String street;
+
+    @Column(name = "Postal_Code", nullable = false, length = 6)
+    private String postalCode;
+
+    @Column(name = "City", nullable = false, length = 30)
+    private String city;
+
+    public Address() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
