05905c859fa3ccf3a99d7bdfab53788e6b09533d
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-03 13:16:54 +0200

    added service in Controller

--- src/main/java/com/example/wigelladminapi/controllers/AdminController.java
+++ src/main/java/com/example/wigelladminapi/controllers/AdminController.java
@@ -1,9 +1,18 @@
 package com.example.wigelladminapi.controllers;
 
+import com.example.wigelladminapi.services.MemberServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    private final MemberServiceInterface memberService;
+
+    @Autowired
+    public AdminController(MemberServiceInterface memberService) {
+        this.memberService = memberService;
+    }
 }
