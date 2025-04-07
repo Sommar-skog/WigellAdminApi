b8fe09762f861873682f18a9cea76cb62ae350f1
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-07 13:46:42 +0200

    Added AddressService and AddressServiceInterface

---
+++ src/main/java/com/example/wigelladminapi/services/AddressServiceInterface.java
@@ -0,0 +1,11 @@
+package com.example.wigelladminapi.services;
+
+import com.example.wigelladminapi.entities.Address;
+
+public interface AddressServiceInterface {
+
+    Address findAdressByStreetAndPostalCodeAndCity(String street, String postalCode, String city);
+    Address findAddressById(Long id);
+    Address addAdress(Address address);
+
+}
