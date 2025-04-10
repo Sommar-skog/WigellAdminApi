d10552651f0f1a38ea770be48c3ba105b2c3e899
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-10 14:24:21 +0200

    småfix

--- src/main/java/com/example/wigelladminapi/services/MemberServiceImpl.java
+++ src/main/java/com/example/wigelladminapi/services/MemberServiceImpl.java
@@ -16,7 +16,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class MemberServiceImpl implements MemberService {
+public class
+MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
     private final AddressServiceImpl addressServiceImpl;
